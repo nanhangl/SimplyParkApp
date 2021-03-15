69aa9a73994ab36b61e293bf6221b157e5d90c96
Nan Hang Lim <190704d@mymail.nyp.edu.sg>
2021-03-15 14:33:16 +0800

    .

--- SimplyPark/src/screens/HomeScreen.js
+++ SimplyPark/src/screens/HomeScreen.js
@@ -1,11 +1,14 @@
 import React, { Component, useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, TextInput,  } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, ScrollView } from 'react-native';
 import MapView, { Circle, Marker } from 'react-native-maps';
 import * as Feather from "react-native-feather";
 import Geolocation from '@react-native-community/geolocation';
 import CarparksArray from '../../assets/carparks_01032021.json';
 import CarparksTest from '../../assets/carparks_test.json';
 import axios from 'axios';
+import _ from 'lodash';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faParking } from '@fortawesome/free-solid-svg-icons'
 
 var proj4 = require('proj4');
 
@@ -13,6 +16,7 @@ const homeScreen = ({navigation}) => {
     const [locationInfo, setLocationInfo] = useState({"coords":{"longitude":1.3521,"latitude":103.8198}});
     const [refreshLocation, setRefreshLocation] = useState('');
     const [carparkAvailability, setCarparkAvailability] = useState(false);
+    const [cpRadius, setCpRadius] = useState(20);
 
     useEffect(async () => {
         Geolocation.getCurrentPosition(info => setLocationInfo(info))
@@ -42,10 +46,10 @@ const homeScreen = ({navigation}) => {
     }
 
     const renderCarparks = cpArray => {
-        
         const cpMarkers = cpArray.map((item) => {
             var coords = proj4("EPSG:3414","EPSG:4326",[item.x_coord,item.y_coord]);
-            if (distance(locationInfo.coords.latitude,locationInfo.coords.longitude,coords[1],coords[0]) <= 20) {
+            var cpDist = distance(locationInfo.coords.latitude,locationInfo.coords.longitude,coords[1],coords[0]);
+            if (cpDist <= cpRadius) {
                 return <Marker
                 key={item.x_coord.toString() + "_" + item.y_coord.toString()}
                 coordinate={{latitude:coords[1],longitude:coords[0]}}
@@ -60,8 +64,50 @@ const homeScreen = ({navigation}) => {
                     </View>
                 </Marker>
         }})
-
         return cpMarkers;
+    }
+
+    const renderNearestCarparks = cpArray => {
+        var nearestCarparksArray = [];
+        var nearestCarparksComponentArray = [];
+        
+        for (var item in cpArray) {
+            var coords = proj4("EPSG:3414","EPSG:4326",[cpArray[item].x_coord,cpArray[item].y_coord]);
+            var cpDist = distance(locationInfo.coords.latitude,locationInfo.coords.longitude,coords[1],coords[0]);
+            if (cpDist <= cpRadius) {
+                var lotsAvailable = 0;
+                try {
+                    lotsAvailable = parseInt(getLotsAvailable(cpArray[item].car_park_no));
+                } catch {}
+
+                if (lotsAvailable == 0) {
+                    nearestCarparksComponentArray.push(
+                        <TouchableOpacity>
+                            <View style={{flexDirection:'row',alignItems:'center',borderBottomColor:'#d0d0d0',borderBottomWidth:1,paddingVertical:10}}>
+                                <FontAwesomeIcon icon={faParking} color="#086EB5" size={25} />
+                                <View style={{marginLeft:15,marginRight:40}}>
+                                    <Text style={styles.medium}>{cpArray[item].address}</Text>
+                                    <Text style={[styles.regular,{marginTop:5,color:'#888'}]}>No lots | {Math.round(cpDist * 10) / 10} km away</Text>
+                                </View>
+                            </View>
+                        </TouchableOpacity>
+                    );
+                } else {
+                    nearestCarparksComponentArray.push(
+                        <TouchableOpacity>
+                            <View style={{flexDirection:'row',alignItems:'center',borderBottomColor:'#d0d0d0',borderBottomWidth:1,paddingVertical:10}}>
+                                <FontAwesomeIcon icon={faParking} color="#086EB5" size={25} />
+                                <View style={{marginLeft:15,marginRight:40}}>
+                                    <Text style={styles.medium}>{cpArray[item].address}</Text>
+                                    <Text style={[styles.regular,{marginTop:5,color:'#888'}]}>{ lotsAvailable } {lotsAvailable == 1 ? 'lot' : 'lots' } | {Math.round(cpDist * 10) / 10} km away</Text>
+                                </View>
+                            </View>
+                        </TouchableOpacity>
+                    );
+                }
+            }
+        }
+        return nearestCarparksArray;
     }
 
     return (
@@ -88,14 +134,19 @@ const homeScreen = ({navigation}) => {
                     { carparkAvailability ? renderCarparks(CarparksArray) : <></> }
                     
                 </MapView>
-                <TouchableOpacity onPress={() => setRefreshLocation(Math.random())} style={{position:'absolute',zIndex:2,backgroundColor:'#fff',bottom:5,right:15,padding:10,paddingLeft:8,paddingBottom:8,shadowColor:"#000",shadowOffset:{width:0,height:4},shadowOpacity:0.25,shadowRadius:8,elevation:8,marginVertical:7.5,borderRadius:10}}>
+                <TouchableOpacity onPress={() => setRefreshLocation(Math.random())} style={{position:'absolute',zIndex:2,backgroundColor:'#fff',bottom:5,right:15,padding:10,paddingLeft:8.5,paddingBottom:8,shadowColor:"#000",shadowOffset:{width:0,height:4},shadowOpacity:0.25,shadowRadius:8,elevation:8,marginVertical:7.5,borderRadius:10}}>
                     <Feather.Navigation width={20} height={20} stroke="#404040" fill="#404040" />
                 </TouchableOpacity>
             </View>
-            <View style={{alignItems:'center'}}>
-                <View style={{backgroundColor:'#f0f0f0',width:'92%',height:50,margin:15,borderRadius:5,alignItems:'center',flexDirection:'row'}}>
+            <View>
+                <View style={{backgroundColor:'#f0f0f0',width:'92%',height:50,margin:15,marginBottom:5,borderRadius:5,alignItems:'center',flexDirection:'row'}}>
                     <Feather.Search width={30} height={30} stroke="#404040" style={{marginLeft:10}} />
                     <Text style={[styles.medium,{fontSize:12,width:'90%',marginLeft:10,color:'#888'}]}>Search by Address, Postal Code, or Carpark No.</Text>
+                </View>
+                <View style={{marginHorizontal:15}}>
+                    <ScrollView>
+                        {carparkAvailability ? renderNearestCarparks(CarparksArray).map(item => item) : <></>}
+                    </ScrollView>
                 </View>
             </View>
         </View>
