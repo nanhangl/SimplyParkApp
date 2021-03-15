a8e6108a3ac14b6a36486fee688a7e9a4f23ab82
Nan Hang Lim <190704d@mymail.nyp.edu.sg>
2021-03-15 16:24:56 +0800

    .

--- SimplyPark/App.js
+++ SimplyPark/App.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { create } from 'react-test-renderer';
 import locationPermissionsScreen from './src/screens/LocationPermissionsScreen';
 import homeScreen from './src/screens/HomeScreen';
+import searchScreen from './src/screens/SearchScreen';
 
 const Stack = createStackNavigator();
 
@@ -13,6 +14,7 @@ const App = () => {
       <Stack.Navigator>
         <Stack.Screen name="Location Permissions" component={locationPermissionsScreen} options={{headerShown:false}} />
         <Stack.Screen name="Home" component={homeScreen} options={{headerShown:false}} />
+        <Stack.Screen name="Search" component={searchScreen} options={{headerShown:false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
