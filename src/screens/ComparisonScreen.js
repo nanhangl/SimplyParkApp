6c2bb81e72772d8c40baa5754a2bc6c21557b3c2
Nan Hang Lim <190704d@mymail.nyp.edu.sg>
2021-03-19 13:35:22 +0800

    able to compare hourly data

--- src/screens/ComparisonScreen.js
+++ src/screens/ComparisonScreen.js
@@ -18,6 +18,7 @@ import { resolvePlugin, resolvePreset } from '@babel/core';
 
   const ComparisonScreen = ({ route, navigation }) => {
     const [dataLoader, setDataLoader] = useState(['none','none']);
+    const [dataSetsAmt, setDataSetsAmt] = useState(route.params["type"] == "Hourly" ? 6 : 24);
     const [graphData, setGraphData] = useState([[],[]]);
     const base_url = "https://api.data.gov.sg/v1/transport/carpark-availability";
     const [showDate, setShowDate] = useState(false);
@@ -32,8 +33,40 @@ import { resolvePlugin, resolvePreset } from '@babel/core';
         setDate(currentDate);
     };
 
-    const getGraph = () => {
-        console.log(date);
+    const getGraph = async () => {
+        setGraphData([[],[]]);
+        setDataLoader(['flex','none']);
+        var requestDateTime = moment(date).tz("Asia/Singapore").format().substr(0,11) + route.params["data"][0][route.params["data"][0].length-1] + ":00+08:00";
+        var requestDateTimeInEpoch = new Date(requestDateTime).getTime();
+
+        if (route.params["type"] == "Hourly") {
+            var dataTimes = [];
+            var dataLots = [];
+            try {
+                for (var interval = 0; interval < 6; interval++) {
+                    const intervalTime = (parseInt(requestDateTimeInEpoch) - (600000*interval)).toFixed();
+                    const intervalDateTime = moment(new Date(parseInt(intervalTime)).toISOString()).tz("Asia/Singapore").format().substring(0,19);
+                    var response = await axios.get(`${base_url}?date_time=${intervalDateTime}`);
+                    const carpark_data = response.data.items[0].carpark_data;
+                    for (var item in carpark_data) {
+                        if (carpark_data[item].carpark_number == route.params["car_park_no"]) {
+                            dataTimes.unshift(intervalDateTime);
+                            dataLots.unshift(parseInt(carpark_data[item].carpark_info[0].lots_available));
+                            break;
+                        }
+                    }
+                }
+                setDataLoader(['none','none']);
+                setGraphData([
+                    [dataTimes[0].substr(11,5),dataTimes[1].substr(11,5),dataTimes[2].substr(11,5),dataTimes[3].substr(11,5),dataTimes[4].substr(11,5),dataTimes[5].substr(11,5)],
+                    dataLots
+                ])
+            } catch {
+                setDataLoader(['none','flex']);
+            }
+        } else {
+
+        }
     }
 
     return (
@@ -86,14 +119,62 @@ import { resolvePlugin, resolvePreset } from '@babel/core';
             <View style={{marginHorizontal:25, marginTop:15,height:'35%'}}>
                 <View style={{flexDirection:'row',alignItems:'center'}}><Text style={[styles.bold, {fontSize:20}]}>Another Day to Compare</Text></View>
                 <View style={{flexDirection:'row',alignItems:'center',marginVertical:5}}>
-                    <Text style={[styles.medium]}>Data for <Text style={[styles.medium,{color:'#007AFF'}]}>{date.toISOString().substr(0,10)}</Text> {route.params["data"][0][0]} to {route.params["data"][0][route.params["data"][0].length-1]}</Text>
+                    <Text style={[styles.medium]}>Data for <Text onPress={() => setShowDate(true)} style={[styles.medium,{color:'#007AFF'}]}>{moment(date.toISOString()).tz("Asia/Singapore").format().substr(0,10)}</Text> {route.params["data"][0][0]} to {route.params["data"][0][route.params["data"][0].length-1]}</Text>
                     <TouchableOpacity onPress={() => getGraph()} style={{backgroundColor:'#007AFF',paddingVertical:5,paddingHorizontal:10,borderRadius:5,marginLeft:'auto'}}><Text style={[styles.bold,{color:'#fff'}]}>Get Chart</Text></TouchableOpacity>
                 </View>
+                {showDate ?
+                    <DateTimePicker
+                    testID="datePicker"
+                    value={date}
+                    mode="date"
+                    is24Hour={true}
+                    display="default"
+                    maximumDate={new Date(parseInt(route.params["requestEpoch"])-86400000)}
+                    onChange={changeDate}
+                    />
+                : <></>}
                 <Text style={[styles.medium, {marginTop:15,display:dataLoader[1]}]}>Cannot Retrieve Data</Text>
                 <View style={{justifyContent:'center',height:'80%',display:dataLoader[0],alignItems:'center'}}>
                     <ActivityIndicator size='large' color="#007AFF" />
                     <Text style={[styles.medium,{marginTop:5}]}>This will take approx. {route.params["type"] == "Hourly" ? "10 secs" : "a min."}</Text>
                 </View>
+                { graphData[0].length == dataSetsAmt ? 
+                 <LineChart 
+                 data={{
+                     labels: graphData[0],
+                     datasets: [
+                         {
+                             data: graphData[1]
+                         }
+                     ]
+                 }}
+                 width={Dimensions.get('window').width-50}
+                 height={200}
+                 yAxisSuffix=" lots"
+                 chartConfig={{
+                     backgroundColor: "#007AFF",
+                     backgroundGradientFrom: "#007AFF",
+                     backgroundGradientTo: "#007AFF",
+                     decimalPlaces: 0, // optional, defaults to 2dp
+                     color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                     labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                     style: {
+                       borderRadius: 16
+                     },
+                     propsForDots: {
+                       r: "6",
+                       strokeWidth: "2",
+                       stroke: "#007AFF"
+                     }
+                   }}
+                   bezier
+                   style={{
+                     marginVertical: 8,
+                     borderRadius: 16
+                   }} />
+                 : 
+                 <></> 
+                 }
             </View>
         </View>
     )
